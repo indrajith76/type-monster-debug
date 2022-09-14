d35895d48ad0e87d1952ab4afa418d029275dc4d
indrajith76 <indrajithgoswami@gmail.com>
2022-09-15 01:04:43 +0600

    fixed result model seconds bug and center the result modal

--- script.js
+++ script.js
@@ -81,13 +81,12 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken.toFixed(0)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
-  console.log(errorCount);
   // restart everything
   startTime = null;
   errorCount = 0;
