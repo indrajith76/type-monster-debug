4786915596efd0cdb270700f68ea081d0d87c1d3
indrajith76 <indrajithgoswami@gmail.com>
2022-09-14 23:55:33 +0600

    fixed mistake count bug

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
   }
 
   // check if given question text is equal to user typed text
@@ -86,7 +87,7 @@ const gameOver = () => {
   `;
 
   addHistory(questionText, timeTaken, errorCount);
-
+  console.log(errorCount);
   // restart everything
   startTime = null;
   errorCount = 0;
